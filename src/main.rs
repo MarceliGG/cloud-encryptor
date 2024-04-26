5243893182c745d35b3b91b4d2e9e6776417f1ea
MarcelGiemza <u80_margie_waw@technischools.com>
2024-04-26 10:51:41 +0200

    file download

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use onedrive_api::{
-    Auth, ClientCredential, DriveLocation, FileName, ItemLocation, OneDrive, Permission, Result,
-    Tenant,
+    Auth, ClientCredential, DriveLocation, ItemLocation, OneDrive, Permission, Tenant,
 };
 use rouille::{router, Response, Server};
 use std::fs;
@@ -11,6 +10,14 @@ static mut CODE: String = String::new();
 
 #[tokio::main]
 async fn main() {
+    println!(
+        "{}",
+        xdg_user::UserDirs::new()
+            .unwrap()
+            .downloads()
+            .unwrap()
+            .display()
+    );
     let cache_file_path = format!(
         "{}/.cache/cloudencryptor/token",
         std::env::var("HOME").unwrap()
@@ -46,6 +53,15 @@ async fn main() {
                     .expect("Error reading from STDIN");
                 upload(input.trim().to_string(), &drive).await;
             }
+            "d" => {
+                print!("File name: ");
+                let mut input: String = String::new();
+                let _ = io::stdout().flush();
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Error reading from STDIN");
+                download(input.trim().to_string(), &drive).await;
+            }
             "q" => break,
             "l" => {
                 token = login(cache_file_path.clone()).await;
@@ -56,22 +72,72 @@ async fn main() {
                 println!("l - log in");
                 println!("u - upload file");
                 println!("f - list files");
+                println!("d - download file");
             }
-            "f" => {
-                let list = drive
-                    .list_children(ItemLocation::from_path("/encrypted").unwrap())
-                    .await;
-                match list {
-                    Ok(l) => {
-                        for i in l.iter() {
-                            println!("{}", i.name.clone().unwrap());
-                        }
-                    }
-                    Err(e) => println!("{}", e),
-                }
-            }
+            "f" => list_files(&drive).await,
             _ => println!("err"),
         }
+    }
+}
+
+async fn download_from_url(url: String, file_name: String) {
+    let mut resp = reqwest::get(url).await.expect("request failed");
+    let mut path = format!(
+        "{}/{}",
+        xdg_user::UserDirs::new()
+            .unwrap()
+            .downloads()
+            .unwrap()
+            .display(),
+        file_name
+    );
+    let mut i = 0;
+    let mut path_f = path.clone();
+    while std::path::Path::new(&path_f).exists() {
+        let p = std::path::Path::new(&path);
+        path_f = format!(
+            "{}/{}{}.{}",
+            xdg_user::UserDirs::new()
+                .unwrap()
+                .downloads()
+                .unwrap()
+                .display(),
+            p.file_stem().unwrap().to_str().unwrap(),
+            i,
+            p.extension().unwrap().to_str().unwrap()
+        );
+        i += 1;
+    }
+    let mut out = fs::File::create(&path_f).unwrap();
+    io::copy(
+        &mut resp.text().await.expect("body invalid").as_bytes(),
+        &mut out,
+    )
+    .expect("failed to copy content");
+    println!("Downloaded to {}", &path_f);
+}
+
+async fn download(file: String, drive: &OneDrive) {
+    match drive
+        .get_item_download_url(ItemLocation::from_path(&format!("/encrypted/{}", file)).unwrap())
+        .await
+    {
+        Ok(l) => download_from_url(l, file).await,
+        Err(e) => println!("{}", e),
+    }
+}
+
+async fn list_files(drive: &OneDrive) {
+    let list = drive
+        .list_children(ItemLocation::from_path("/encrypted").unwrap())
+        .await;
+    match list {
+        Ok(l) => {
+            for i in l.iter() {
+                println!("{}", i.name.clone().unwrap());
+            }
+        }
+        Err(e) => println!("{}", e),
     }
 }
 
@@ -96,7 +162,7 @@ async fn upload(file: String, drive: &OneDrive) {
         Err(_) => {
             println!("could not read file")
         }
-    }
+    };
 }
 
 async fn login(cache_file: String) -> String {
